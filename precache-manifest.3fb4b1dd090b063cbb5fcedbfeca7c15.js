b2c334ed84f712ef95bd1451ec6e7dd50f78edd1
Pedro Gesta <pedrogestajr@gmail.com>
2020-09-10 02:18:40 +1000

    Updates

---
+++ precache-manifest.3fb4b1dd090b063cbb5fcedbfeca7c15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b18cf7294579f17b7831f6f40d42e7",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "592426242f5c2ec9d7bb",
+    "url": "/covid-19/static/css/main.6d727011.chunk.css"
+  },
+  {
+    "revision": "1d9276318a08d4e2d1ea",
+    "url": "/covid-19/static/js/2.dbdec339.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.dbdec339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592426242f5c2ec9d7bb",
+    "url": "/covid-19/static/js/main.66771e92.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "599e0ce06c83082939a0a003cefa9d33",
+    "url": "/covid-19/static/media/dna-body.599e0ce0.png"
+  },
+  {
+    "revision": "00ddbf3f9e1e664d0cf8cc96d3379878",
+    "url": "/covid-19/static/media/social-distancing.00ddbf3f.png"
+  },
+  {
+    "revision": "b5eeaa5cbd0399a42438f867b731d030",
+    "url": "/covid-19/static/media/wash-hands.b5eeaa5c.png"
+  }
+]);
